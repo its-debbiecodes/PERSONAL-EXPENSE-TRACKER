dec5301a02234f07fb839efcf8876924eb83ade1
its-debbiecodes <debbieabegunde23@gmail.com>
2026-08-29 21:21:41 +0100

    Initial Commit

---
+++ file_handler.py
@@ -0,0 +1,10 @@
+
+import json
+expense_tracker=[]
+try:
+    with open('personal_expense_tracker.json','r') as f:
+        expense_tracker=json.load(f)
+except FileNotFoundError:
+    print('File not found, starting from scratch')
+except json.decoder.JSONDecodeError:
+    print('File not found, starting from scratch')
